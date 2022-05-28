bfbd05dec57276a4860b274f3c4ed9c142f98715
JeffTam33 <53273407+JeffTam33@users.noreply.github.com>
2022-05-27 20:21:49 -0400

    Beginning of Design

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul id="navbar">
+          <li><a class="nav-link" href="#page1"><p>page1</p></a></li>
+          <li><a class="nav-link" href="#page2"><p>page2</p></a></li>
+          <li><a class="nav-link" href="#page3"><p>page3</p></a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
